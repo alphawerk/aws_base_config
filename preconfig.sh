9563b5774f70c1f19b861537eaa06088792a1a3b
mattbrain <matt.brain@gmail.com>
2021-03-04 16:24:20 +0000

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -7,3 +7,8 @@ echo "Version 0.0.3"
 
 sudo apt -y update
 sudo apt -y upgrade
+sudo apt -y install docker.io
+
+sudo wget -O /usr/bin/fetch https://github.com/gruntwork-io/fetch/releases/download/v0.4.1/fetch_linux_amd64
+sudo chmod 755 /usr/bin/fetch
+
