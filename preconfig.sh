cedf0e8dbc769e66a5a96f6b6f48e6855e18ffa3
mattbrain <matt.brain@gmail.com>
2021-05-11 19:58:11 +0100

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -11,7 +11,7 @@ sudo apt-get -y install docker.io docker-compose
 
 sudo wget -O /usr/bin/fetch https://github.com/gruntwork-io/fetch/releases/download/v0.4.1/fetch_linux_amd64
 sudo chmod 755 /usr/bin/fetch
-
+sudo apt install apache2-utils
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
